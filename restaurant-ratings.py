2a2d0b844f2d381a8732d4bc4013f2ba88112c16
Rosie <rosie@riveter16-md.swna.wdpr.disney.com>
2016-04-11 17:34:07 -0700

    Add input for new ratings and sort function

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,10 +1,8 @@
 import sys
 
-
+ratings_dictionary = {}
 
 def make_dictionary(scores_file):
-
-	ratings_dictionary = {}
 	
 	for line in open(scores_file):
 		line = line.rstrip()
@@ -13,8 +11,26 @@ def make_dictionary(scores_file):
 		rating = int(data[1])
 		ratings_dictionary[restaurant] = rating
 
-	for restaurant, rating in ratings_dictionary.items():
-		print "%s is rated at %d." % (restaurant, rating)
+# def print_dictionary():
+# 	for restaurant, rating in sorted_dictionary.items():
+# 		print "%s is rated at %d." % (restaurant, rating)
+# 	print "\n"
+
+def sort_dictionary():
+	sorted_dictionary = sorted(ratings_dictionary)
+	for restaurant in sorted_dictionary:
+		print "%s is rated at %d." % (restaurant, ratings_dictionary[restaurant])
+	print "\n"
 
 scores_file = sys.argv[1]
 make_dictionary(scores_file)
+sort_dictionary()
+
+restaurant_name = raw_input('Please add a new restaurant: ')
+new_restaurant_rating = int(raw_input('Now, rate it: '))
+
+ratings_dictionary[restaurant_name] = new_restaurant_rating
+
+sort_dictionary()
+
+
